89ee29d2b8d24f12946d274fc193030582e904a4
19h47 <jeremylevron@19h47.fr>
2020-06-07 22:39:15 +0200

    :arrow_up: Upgrade dependencies

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -5,7 +5,7 @@
  * @return str
  * @see https://github.com/ReactTraining/history/blob/master/modules/createHashHistory.js#L38
  */
-export default function (href) {
+export default function getHash (href) {
 	const hashIndex = href.indexOf('#');
 
 	return -1 === hashIndex ? '' : href.substring(hashIndex + 1);
