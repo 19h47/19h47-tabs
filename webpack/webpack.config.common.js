982fd5312f7eab8e7d87d2796abe39dba7c9542e
19h47 <jeremylevron@19h47.fr>
2020-10-17 22:44:49 +0200

    :pencil: Add favicon

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -8,6 +8,7 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const resolve = require('./webpack.utils');
 
@@ -63,6 +64,9 @@ module.exports = {
 			title: 'Webpack',
 			excludeWarnings: true,
 			alwaysNotify: true,
+		}),
+		new CopyPlugin({
+			patterns: [{ from: resolve('open-file-folder.png'), to: resolve('docs/') }],
 		}),
 	],
 };
